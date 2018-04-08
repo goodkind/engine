972cb73a62d05625ced4560467a7bf9a90cb4233
Thomas ter Wijlen <t.ter.wijlen@gmail.com>
2018-04-08 15:43:18 +0200

    Added basic screen rendering and a more advanced game loop with fps count

--- src/com/decre/main/Game.java
+++ src/com/decre/main/Game.java
@@ -2,8 +2,14 @@ package com.decre.main;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
+
+import com.decre.main.graphics.Screen;
 
 public class Game extends Canvas implements Runnable {
 
@@ -13,15 +19,23 @@ public class Game extends Canvas implements Runnable {
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 
+	public static final String TITLE = "Reign";
+
 	private Thread thread;
 	private JFrame frame;
 	private boolean running;
+
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
 
 		frame = new JFrame();
+		screen = new Screen(width, height);
 	}
 
 	public synchronized void start() {
@@ -41,10 +55,62 @@ public class Game extends Canvas implements Runnable {
 
 	@Override
 	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1_000_000_000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+
 		while (running) {
-			System.out.println("Running...");
+			long now = System.nanoTime();
+
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
+
+			render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println(updates + " ups, " + frames + " fps");
+				frame.setTitle(TITLE + " " + updates + " ups, " + frames + " fps");
+				frames = 0;
+				updates = 0;
+			}
+		}
+		stop();
+	}
+
+	public void update() {
+
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		screen.clear();
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
 		}
 
+		Graphics g = bs.getDrawGraphics();
+
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+
+		g.dispose();
+		bs.show();
 	}
 
 	public static void main(String[] args) {
@@ -56,7 +122,7 @@ public class Game extends Canvas implements Runnable {
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
 
