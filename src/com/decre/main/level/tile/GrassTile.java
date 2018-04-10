fbf0ac367dcd08a859f3d51c38690d3fe3b6a564
Thomas ter Wijlen <t.ter.wijlen@gmail.com>
2018-04-10 18:20:30 +0200

    Set up the basics for creating tiles

---
+++ src/com/decre/main/level/tile/GrassTile.java
@@ -0,0 +1,17 @@
+package com.decre.main.level.tile;
+
+import com.decre.main.graphics.Screen;
+import com.decre.main.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	@Override
+	public void render(int x, int y, Screen screen) {
+		// TODO render grass tile here
+	}
+
+}
--- src/com/decre/main/level/tile/Tile.java
+++ src/com/decre/main/level/tile/Tile.java
@@ -4,18 +4,20 @@ import com.decre.main.graphics.Screen;
 import com.decre.main.graphics.Sprite;
 
 public class Tile {
-	
+
 	public int x, y;
 	public Sprite sprite;
-	
+
+	public static Tile grass = new GrassTile(Sprite.grass);
+
 	public Tile(Sprite sprite) {
 		this.sprite = sprite;
 	}
-	
+
 	public void render(int x, int y, Screen screen) {
-		
+
 	}
-	
+
 	public boolean solid() {
 		return false;
 	}
